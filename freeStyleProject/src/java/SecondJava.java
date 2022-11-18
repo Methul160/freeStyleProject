d3d128cadd10ae652fb3029c483a2e84d5923bcc
Methul160 <66194588+Methul160@users.noreply.github.com>
2022-11-18 13:21:26 +0530

    Update SecondJava.java

--- freeStyleProject/src/java/SecondJava.java
+++ freeStyleProject/src/java/SecondJava.java
@@ -2,7 +2,7 @@ package java;
 
 public class SecondJava {
 	public static void main(String[] args) {
-		System.out.println("Automation");	
+		System.out.println("Automation Bro Only Reech");	
 
 		}
 
